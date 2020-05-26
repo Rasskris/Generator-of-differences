9a0218cd3a0261dc61c3917cf4af1b887c262354
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-05-26 19:46:26 +0300

    remove option

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,6 @@ import program from 'commander';
 
 program
   .version('1.0.0', '-V, --version', 'output usage information')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-h, --help ', 'output usage information');
+  .description('Compares two configuration files and shows a difference.');
 
 program.parse(process.argv);

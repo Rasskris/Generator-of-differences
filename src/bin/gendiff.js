f5b317c4637d4359376cf9a8d4ab8dd1bfda894c
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-05-26 18:55:26 +0300

    update file

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,7 +5,6 @@ import program from 'commander';
 program
   .version('1.0.0', '-V, --version', 'output usage information')
   .description('Compares two configuration files and shows a difference.')
-  .option('-h, --help', 'output extra debugging')
-  .option('-V, --version', 'output usage information');
+  .option('-h, --help ', 'output usage information');
 
 program.parse(process.argv);

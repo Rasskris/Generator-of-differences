e49ce676b4c081477964ea6fbf8c7058c640971e
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-05-27 13:14:06 +0300

    step 2: add 'option', 'arguments', 'action' to gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,6 +4,9 @@ import program from 'commander';
 
 program
   .version('1.0.0', '-V, --version', 'output usage information')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action();
 
 program.parse(process.argv);

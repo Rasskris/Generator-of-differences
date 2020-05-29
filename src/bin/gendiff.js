ed0a428b2c176963a4c198afb5c23e26f090a809
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-05-29 15:45:46 +0300

    step 3: add genDiff to action

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import genDiff from '..';
 
 program
   .version('1.0.0', '-V, --version', 'output usage information')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action();
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse(process.argv);

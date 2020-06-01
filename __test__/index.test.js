be1b2d935c0d8d2b71d5c8d67a84582fa27ef0a5
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-06-01 17:57:11 +0300

    add test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,2 +1,17 @@
-import genDiff from '../src/index.js';
+import path from 'path';
+import genDiff from '../src';
 
+test('genDiff', () => {
+  const getPath = (filename) => path.join(__dirname, `__fixtures__${filename}`);
+  const before = getPath('before.json');
+  const after = getPath('after.json');
+  const result = `{
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  - follow: false
+  + verbose: true
+ }`;
+  expect(genDiff(before, after)).toEqual(result);
+});

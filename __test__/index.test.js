4600ff45602660067c3fe9f42ed9b78070fd71c4
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-06-03 17:07:50 +0300

    step 6: refactor index.test.js

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,10 +1,13 @@
 import path from 'path';
 import genDiff from '../src';
 
-test('genDiff', () => {
+const filetypes = ['json', 'yml', 'ini'];
+const filetype = filetypes.flatMap((type) => type);
+
+test.each(filetype)('find some difference %s', (type) => {
   const getPath = (filename) => path.join(__dirname, `__fixtures__/${filename}`);
-  const before = getPath('before.yml');
-  const after = getPath('after.yml');
+  const before = getPath(`before.${type}`);
+  const after = getPath(`after.${type}`);
   const result = `{ 
     host: hexlet.io
   + timeout: 20

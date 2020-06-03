d5959c870eb185b309ff7a1bea6f0f97ba2286f6
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-06-03 17:09:50 +0300

    step 6:  add ini.parse to parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,9 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parsers = {
   json: JSON.parse,
   yml: yaml.safeLoad,
+  ini: ini.parse,
 };
 export default (type, content) => parsers[type](content);

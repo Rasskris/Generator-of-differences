98dacc422889cbfa24e1baaf29aabdcaab527bf8
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-06-12 19:04:32 +0300

    step 7: add fileFormats to index.js

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,20 +1,18 @@
+import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
 const filetypes = ['json', 'yml', 'ini'];
-const filetype = filetypes.flatMap((type) => type);
+const fileFormats = ['stylish'];
 
-test.each(filetype)('find some difference %s', (type) => {
-  const getPath = (filename) => path.join(__dirname, `__fixtures__/${filename}`);
+const args = fileFormats.flatMap((format) => (
+  filetypes.map((type) => [type, format])
+));
+
+test.each(args)('find some difference %s', (type, format) => {
+  const getPath = (filename) => path.join(__dirname, '__fixtures__', filename);
   const before = getPath(`before.${type}`);
   const after = getPath(`after.${type}`);
-  const result = `{ 
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  - follow: false
-  + verbose: true
- }`;
+  const result = fs.readFileSync(getPath(format), 'utf-8');
   expect(genDiff(before, after)).toEqual(result);
 });

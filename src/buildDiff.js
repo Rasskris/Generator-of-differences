245c84f897f0c23300ecc42dcb19c1e54366d0c8
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-06-22 17:43:35 +0300

    fix buildDiff.js

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,40 +1,28 @@
-import { has, isObject, union } from 'lodash';
+import pkg from 'lodash';
 
-const nodes = [
-  {
-    check: (key, data1, data2) => data1[key] === data2[key],
-    process: (key, value) => ({ key, type: 'unchanged', value }),
-  },
-  {
-    check: (key, data1) => !has(data1, key),
-    process: (key, _value1, value2) => ({ key, type: 'added', value2 }),
-  },
-  {
-    check: (key, _data1, data2) => !has(data2, key),
-    process: (key, value) => ({ key, type: 'removed', value }),
-  },
-  {
-    check: (key, data1, data2) => data1[key] !== data2[key],
-    process: (key, value1, value2) => ({
-      key,
-      type: 'changed',
-      valueBefore: value1,
-      valueAfter: value2,
-    }),
-  },
-  {
-    check: (key, data1, data2) => isObject(data1[key]) && isObject(data2[key]),
-    process: (key, value1, value2, fn) => ({ key, type: 'nested', children: fn(value1, value2) }),
-  },
-];
-
-const getNode = (key, data1, data2) => nodes.find(({ check }) => check(key, data1, data2));
+const { has, isObject, union } = pkg;
 
 const buildDiff = (data1, data2) => {
-  const keys = union(Object.keys(data1), Object.keys(data2));
-  return keys.map((key) => {
-    const { process } = getNode(key, data1, data2);
-    return process(key, data1[key], data2[key]);
+  const unionKey = union(Object.keys(data1), Object.keys(data2)).sort();
+  return unionKey.map((key) => {
+    if (!has(data1, key)) {
+      return { key, type: 'added', value: data2[key] };
+    }
+    if (!has(data2, key)) {
+      return { key, type: 'removed', value: data1[key] };
+    }
+    if (isObject(data1[key]) && isObject(data2[key])) {
+      return { key, type: 'nested', children: buildDiff(data1[key], data2[key]) };
+    }
+    if (data1[key] === data2[key]) {
+      return { key, type: 'unchanged', value: data1[key] };
+    }
+    return {
+      key,
+      type: 'changed',
+      valueBefore: data1[key],
+      valueAfter: data2[key],
+    };
   });
 };
 

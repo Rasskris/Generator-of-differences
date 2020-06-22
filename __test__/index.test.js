f2fcd96b559dbc319b5b7f7ed2d1ddb6c65e0c09
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-06-22 17:40:06 +0300

    fix index.test.js

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,18 +1,18 @@
 import fs from 'fs';
 import path from 'path';
-import genDiff from '../src';
+import genDiff from '../src/index.js';
 
 const filetypes = ['json', 'yml', 'ini'];
-const fileFormats = ['stylish'];
+const fileFormats = ['stylish', 'plain', 'json'];
 
 const args = fileFormats.flatMap((format) => (
   filetypes.map((type) => [type, format])
 ));
 
-test.each(args)('find some difference %s', (type, format) => {
-  const getPath = (filename) => path.join(__dirname, '__fixtures__', filename);
+test.each(args)('%s type files difference with %s output', (type, format) => {
+  const getPath = (filename) => path.resolve('__test__', '__fixtures__', filename);
   const before = getPath(`before.${type}`);
   const after = getPath(`after.${type}`);
   const result = fs.readFileSync(getPath(format), 'utf-8');
-  expect(genDiff(before, after)).toEqual(result);
+  expect(genDiff(before, after, format)).toEqual(result);
 });

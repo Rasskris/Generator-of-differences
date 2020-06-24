916999dddb820e1984f280105966f08b5a72b5a1
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-06-24 15:00:04 +0300

    fix code review remarks

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const fileType1 = getType(filepath1);
   const fileType2 = getType(filepath2);
 
-  const fileContant1 = getContent(filepath1);
-  const fileContant2 = getContent(filepath2);
+  const fileContent1 = getContent(filepath1);
+  const fileContent2 = getContent(filepath2);
 
-  const data1 = parser(fileType1, fileContant1);
-  const data2 = parser(fileType2, fileContant2);
+  const data1 = parser(fileType1, fileContent1);
+  const data2 = parser(fileType2, fileContent2);
 
   const diff = buildDiff(data1, data2);
   return render(diff, format);

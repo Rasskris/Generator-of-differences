5f7b04e33c9541740d91ed9d60c9202cd95d6edf
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-06-29 16:17:55 +0300

    change foo name from parser to parse

--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,9 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const parser = {
+const parse = {
   json: JSON.parse,
   yml: yaml.safeLoad,
   ini: ini.parse,
 };
-export default (type, content) => parser[type](content);
+export default (type, content) => parse[type](content);
